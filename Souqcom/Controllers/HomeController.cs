77828955d47bc3676f78b8ef11e51003323c1611
mahmo <mahmo@MAHMOUD>
2023-02-09 02:16:22 +0200

    Add project files.

---
+++ Souqcom/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Souqcom.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Souqcom.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        EcommerceContext db = new EcommerceContext();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            ViewBag.Review = db.Reviews.ToList();
+            ViewBag.product = db.Products.Take(8).ToList();
+            ViewBag.category = db.Categories.Take(3).ToList();
+                 
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Category()
+        {
+            var cat = db.Categories.ToList();
+            return View(cat);
+        }
+        public IActionResult Product()
+        {
+
+            var prod = db.Products.ToList();
+            return View(prod);
+        }
+        public IActionResult Productcat(int id)
+        {
+            var prod = db.Products.Where(x => x.Catid == id).ToList();
+            return View(prod);
+        }
+        public IActionResult Productsearch(string xname)
+        {
+            var prod = db.Products.Where(x => x.Name.Contains(xname)).ToList();
+            return View("Product", prod);
+        }
+        public IActionResult Review()
+        {
+            var rev = db.Reviews.ToList();
+            return View(rev);
+        }
+
+        [HttpPost]
+        public IActionResult Savereview(Review model)
+        {
+
+            db.Reviews.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Productdetails(int id)
+        {
+            ViewBag.product = db.Products.Where(x => x.Id == id).ToList();
+            var pord = db.ProductImages.Where(x => x.ProductId == id).ToList();
+            return View(pord);
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Souqcom/Controllers/ProductsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Souqcom.Models;
+
+namespace Souqcom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly EcommerceContext db =new EcommerceContext();
+
+        //public ProductsController(EcommerceContext context)
+        //{
+        //    db = context;
+        //}
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var ecommerceContext = db.Products.Include(p => p.Cat);
+            return View(await ecommerceContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,Catid,Photo,Type,SupplierName,EntryDate,ReviewUrl,Quantity,Priceafterdiscount")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Catid,Photo,Type,SupplierName,EntryDate,ReviewUrl,Quantity,Priceafterdiscount")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(product);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await db.Products.FindAsync(id);
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Productsearch(string xname)
+        {
+            var prod = db.Products.Where(x => x.Name.Contains(xname)).ToList();
+            return View("Index", prod);
+        }
+        private bool ProductExists(int id)
+        {
+            return db.Products.Any(e => e.Id == id);
+        }
+        
+    }
+}
